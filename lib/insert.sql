36f0512d25c44f32515e46b887065f32ba58f0e4
sulfranc <sulfranc@gmail.com>
2022-12-01 12:32:59 -0500

    done

---
+++ lib/insert.sql
@@ -0,0 +1,25 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Maru', 3, 'M', 'black', 'calm', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Rowdy', 7, 'M', 'brown', 'cool', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Tabitha', 23, 'F', 'white', 'nice', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Sergeant Brown', 6, 'M', 'brown', 'loud', false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Melissa', 28, 'F', 'black', 'quiet', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Grinch', 9, 'M', 'green', 'angry', true);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ('Wendy', 13, 'F', 'red', 'happy', false);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) 
+VALUES ( NULL, NULL, Null, 'orange', Null, true);
+
+
